36b3e8240ecfa4b0932e66134f61de01d631b553
frederico <107966984+Poliester2005@users.noreply.github.com>
2024-05-20 21:16:28 -0300

    Progesso em adicionar novas profissões

--- src/Components/Admin/AdminAdd.jsx
+++ src/Components/Admin/AdminAdd.jsx
@@ -4,13 +4,13 @@ import axios from "axios";
 export default function AdminAdd() {
   const [clinica, setClinica] = useState(null);
   const [nome, setNome] = useState("");
-  const [verificador, setVerificador] = useState(false);
-  const [nivel, setNivel] = useState("");
+  const [verificador, setVerificador] = useState(false); // Alterado para iniciar como false diretamente
+  const [nivel, setNivel] = useState("1"); // Corrigido para iniciar com 'Operador'
 
   useEffect(() => {
     axios
       .get(
-        `http://localhost:3000/Clinica?id=${localStorage.getItem("idClinica")}`
+        `http://localhost:3000/Clinica/${localStorage.getItem("idClinica")}`
       )
       .then((response) => {
         setClinica(response.data);
@@ -20,7 +20,7 @@ export default function AdminAdd() {
       });
   }, []);
 
-  const addProf = (e) => {
+  const addProf = async (e) => {
     e.preventDefault();
 
     const changes = {};
@@ -34,24 +34,22 @@ export default function AdminAdd() {
     }
 
     if (nivel) {
-      changes.nivel = [...(clinica?.nivel || []), nivel];
+      changes.nivel = [...(clinica?.nivel || []), nivel]; // Corrigido para tratar 'nivel' como uma única escolha
     }
 
-    axios
-      .put(
-        `http://localhost:3000/Clinica?id=${localStorage.getItem("idClinica")}`,
-        changes
-      )
-      .then((response) => {
-        console.log("Dados da clínica atualizados com sucesso:", response.data);
-        setClinica(response.data);
-      })
-      .catch((error) => {
-        console.error("Erro ao atualizar os dados da clínica:", error);
-      });
-  };
+    console.log(clinica);
 
-  console.log(clinica);
+    try {
+      const response = await axios.put(
+        `http://localhost:3000/Clinica/${localStorage.getItem("idClinica")}`,
+        changes
+      );
+      console.log("Dados da clínica atualizados com sucesso:", response.data);
+      setClinica(response.data);
+    } catch (error) {
+      console.error("Erro ao atualizar os dados da clínica:", error);
+    }
+  };
 
   return (
     <>
@@ -62,7 +60,7 @@ export default function AdminAdd() {
           onChange={(e) => setVerificador(e.target.checked)}
           checked={verificador}
         />
-        <select onChange={(e) => setNivel(e.target.value)}>
+        <select value={nivel} onChange={(e) => setNivel(e.target.value)}>
           <option value="1">Operador</option>
           <option value="2">Profissional</option>
           <option value="3">Estagiario</option>
