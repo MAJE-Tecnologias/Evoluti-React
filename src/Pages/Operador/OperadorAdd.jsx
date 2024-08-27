8c98513992122b2dc963ee97c3bac9b505c13293
RenatoAC2004 <108144847+RenatoAC2004@users.noreply.github.com>
2024-08-26 22:06:26 -0300

    feat: Design da tela OperadorAdd

--- src/Pages/Operador/OperadorAdd.jsx
+++ src/Pages/Operador/OperadorAdd.jsx
@@ -1,11 +1,15 @@
 // Importações de componentes e ícones necessários
 import { useRef, useEffect, useState } from "react";
 
-import Sidebar, {
-  ItemsSidebar,
-} from "../../Components/SideBar";
+import Sidebar, { ItemsSidebar } from "../../Components/SideBar";
 import "../CSS/AnimacaoFlutuar.css";
-import { FaUsers, FaUserInjured, FaFileAlt, FaUserCheck } from "react-icons/fa";
+import {
+  FaUsers,
+  FaUserInjured,
+  FaFileAlt,
+  FaUserCheck,
+  FaCamera,
+} from "react-icons/fa";
 import { VscGraph } from "react-icons/vsc";
 import axios from "axios";
 
@@ -57,11 +61,12 @@ export default function OperadorAdd() {
       fk_clinica: idClinica,
     };
 
-    axios.post("http://localhost:3000/Paciente", body)
-        .then(() => {
-          alert("Cadastrado com sucesso");
-        })
-        .catch((error) => console.log("error", error));
+    axios
+      .post("http://localhost:3000/Paciente", body)
+      .then(() => {
+        alert("Cadastrado com sucesso");
+      })
+      .catch((error) => console.log("error", error));
   };
 
   return (
@@ -85,93 +90,168 @@ export default function OperadorAdd() {
 
       {/* Seção principal do formulário de adição de paciente */}
       <section
-        style={{
-          position: "absolute",
-          left: "50%",
-          transform: "translateX(-50%)",
-        }}
+        id="FuncHome"
+        className="flex md:flex-col flex-col h-full pl-[78px] justify-center items-center dark:bg-neutral-800 transition-all"
       >
-        <form onSubmit={createPaciente}>
-          <h1>Criar novo paciente</h1>
-          <label htmlFor="nome">Nome:</label>
-          <input
-            type="text"
-            id="nome"
-            name="nome"
-            onChange={(e) => setNome(e.target.value)}
-          />
-          <br />
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <br />
-          <label htmlFor="cpf">CPF:</label>
-          <input
-            type="text"
-            id="cpf"
-            name="cpf"
-            onChange={(e) => setCPF(e.target.value)}
-          />
-          <br />
-          <label htmlFor="rg">RG:</label>
-          <input
-            type="text"
-            id="rg"
-            name="rg"
-            onChange={(e) => setRG(e.target.value)}
-          />
-          <br />
-          <label htmlFor="dataNascimento">Data de Nascimento:</label>
-          <input
-            type="date"
-            id="dataNascimento"
-            name="dataNascimento"
-            onChange={(e) => setData(e.target.value)}
-          />
-          <br />
-          <label htmlFor="cadastroGeneroClinica" className="dark:text-white">
-            Gênero
-          </label>
-          <select
-            id="cadastroGeneroClinica"
-            name="genero"
-            onChange={(e) => setGenero(e.target.value)}
-            className="w-full bg-loginButtonsBackground 
-                            border border-evolutiLightGreen 
-                            p-3.5 rounded-lg shadow-md focus:outline-evolutiGreenDarker"
+        <div className="w-full h-full py-10 md:px-10 md:pt-10">
+          <h1 className="flex flex-col justify-center items-center gap-x-2 text-4xl text-center font-extrabold text-evolutiLightGreen pb-10">
+            <FaUserCheck size={40} />
+            <span>Adicionar Pacientes</span>
+          </h1>
+          <div
+            className="bg-neutral-100 flex flex-col border-2 border-b-0 w-full h-fit rounded-bl-none rounded-br-none
+          shadow-black shadow-md p-5 gap-x-8 md:gap-x-0 md:flex-row md:rounded-3xl dark:bg-neutral-900 dark:border-gray-800"
           >
-            <option defaultValue="">Selecione o gênero</option>
-            <option value="1" className="text-black">
-              Homem-Cis
-            </option>
-            <option value="2" className="text-black">
-              Mulher-Cis
-            </option>
-            <option value="3" className="text-black">
-              Homem-Trans
-            </option>
-            <option value="4" className="text-black">
-              Mulher-Trans
-            </option>
-            <option value="5" className="text-black">
-              Outro
-            </option>
-          </select>
-          <br />
-          <label htmlFor="telefone">Telefone:</label>
-          <input
-            type="text"
-            id="telefone"
-            name="telefone"
-            onChange={(e) => setTelefone(e.target.value)}
-          />
-          <br />
-          <input type="submit" />
-        </form>
+            <form onSubmit={createPaciente}>
+              <div className="flex justify-center pb-5">
+                <div className="relative flex flex-col items-center justify-center border border-gray-300 rounded-full w-28 h-28 shadow-md cursor-pointer group">
+                  <div
+                    className="bg-black w-28 h-28 rounded-full text-white flex flex-col items-center justify-center
+                  transition-opacity duration-300 opacity-0 group-hover:opacity-100 bg-opacity-50 z-10"
+                  >
+                    <FaCamera size={20} className="text-white" />
+                    <span className="text-xs ">Adicionar Foto</span>
+                  </div>
+                  <img
+                    src="https://picsum.photos/112"
+                    className="absolute w-28 h-28 rounded-full"
+                    alt="default image"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-y-4">
+                <div className="flex flex-col gap-y-2">
+                  <label htmlFor="nome" className="font-bold">
+                    Nome:
+                  </label>
+                  <input
+                    type="text"
+                    id="nome"
+                    name="nome"
+                    className="w-full bg-loginButtonsBackground 
+                            border border-evolutiLightGreen placeholder:text-evolutiGreenDarker
+                            p-2 rounded-lg shadow-md focus:outline-evolutiGreenDarker"
+                    placeholder="Insira o nome do paciente"
+                    onChange={(e) => setNome(e.target.value)}
+                  />
+                </div>
+                <div className="flex flex-col gap-y-2">
+                  <label htmlFor="email" className="font-bold">
+                    Email:
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full bg-loginButtonsBackground 
+                border border-evolutiLightGreen placeholder:text-evolutiGreenDarker
+                p-2 rounded-lg shadow-md focus:outline-evolutiGreenDarker"
+                    placeholder="Insira o email do paciente"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+
+                <div className="flex flex-col gap-y-2">
+                  <label htmlFor="cpf" className="font-bold">
+                    CPF:
+                  </label>
+                  <input
+                    type="text"
+                    id="cpf"
+                    name="cpf"
+                    className="w-full bg-loginButtonsBackground 
+                border border-evolutiLightGreen placeholder:text-evolutiGreenDarker
+                p-2 rounded-lg shadow-md focus:outline-evolutiGreenDarker"
+                    placeholder="Insira o CPF do paciente"
+                    onChange={(e) => setCPF(e.target.value)}
+                  />
+                </div>
+
+                <div className="flex flex-col gap-y-2">
+                  <label htmlFor="rg" className="font-bold">
+                    RG:
+                  </label>
+                  <input
+                    type="text"
+                    id="rg"
+                    name="rg"
+                    className="w-full bg-loginButtonsBackground 
+                border border-evolutiLightGreen placeholder:text-evolutiGreenDarker
+                p-2 rounded-lg shadow-md focus:outline-evolutiGreenDarker"
+                    placeholder="Insira o RG do paciente"
+                    onChange={(e) => setRG(e.target.value)}
+                  />
+                </div>
+
+                <div className="flex flex-col gap-y-2">
+                  <label htmlFor="dataNascimento" className="font-bold">
+                    Data de Nascimento:
+                  </label>
+                  <input
+                    type="date"
+                    id="dataNascimento"
+                    name="dataNascimento"
+                    className="w-full bg-loginButtonsBackground text-evolutiGreenDarker
+                border border-evolutiLightGreen placeholder:text-evolutiGreenDarker
+                p-2 rounded-lg shadow-md focus:outline-evolutiGreenDarker"
+                    onChange={(e) => setData(e.target.value)}
+                  />
+                </div>
+
+                <div className="flex flex-col gap-y-2">
+                  <label htmlFor="cadastroGeneroClinica" className="font-bold">
+                    Gênero
+                  </label>
+                  <select
+                    id="cadastroGeneroClinica"
+                    name="genero"
+                    onChange={(e) => setGenero(e.target.value)}
+                    className="w-full bg-loginButtonsBackground 
+                            border border-evolutiLightGreen text-evolutiGreenDarker
+                            p-2 rounded-lg shadow-md focus:outline-evolutiGreenDarker"
+                  >
+                    <option defaultValue="">Selecione o gênero</option>
+                    <option value="1" className="text-black">
+                      Homem-Cis
+                    </option>
+                    <option value="2" className="text-black">
+                      Mulher-Cis
+                    </option>
+                    <option value="3" className="text-black">
+                      Homem-Trans
+                    </option>
+                    <option value="4" className="text-black">
+                      Mulher-Trans
+                    </option>
+                    <option value="5" className="text-black">
+                      Outro
+                    </option>
+                  </select>
+                </div>
+
+                <div className="flex flex-col gap-y-2">
+                  <label htmlFor="telefone" className="font-bold">Telefone:</label>
+                  <input
+                    type="text"
+                    id="telefone"
+                    name="telefone"
+                    className="w-full bg-loginButtonsBackground 
+                border border-evolutiLightGreen placeholder:text-evolutiGreenDarker
+                p-2 rounded-lg shadow-md focus:outline-evolutiGreenDarker"
+                    placeholder="Insira o nome do paciente"
+                    onChange={(e) => setTelefone(e.target.value)}
+                  />
+                </div>
+                <div className="flex justify-center">
+                  <input
+                    type="submit"
+                    className="px-8 py-1 bg-evolutiGreen text-white rounded-md font-semibold"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </section>
     </>
   );
