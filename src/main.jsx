d781eb4f811ae946937c90dbaa7d551094a8c362
Eduardo Pielich <107966984+Poliester2005@users.noreply.github.com>
2024-09-27 09:45:12 -0300

    feat: provider Pontos de dor

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./Contexts/UserProvider.jsx";
+import { PontosProvider } from "Contexts/PontosProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
